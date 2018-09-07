ab03ab84d7fdc03c21bf96574108f0b75c2e46d6
Ugo Aniukwu <ugo@smallhaxe.com>
2018-09-07 21:28:12 +0000

    button.js

---
+++ src/js/button.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const submitButton = document.getElementById('submit-button')
+
+submitButton.addEventListener('click', function() {
+    console.log('User just tried to submit. Woop woop!')
+})
